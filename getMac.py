df1a6aa3cbacc9d3e48225b23fda7f392d4c5800
cayLouis <815569735@qq.com>
2019-11-17 15:01:04 +0800

    modified

--- getMac.py
+++ getMac.py
@@ -1,9 +1,10 @@
 from scapy.all import *
+import sys
 
 localmac = "b0:35:9f:21:5e:4e"
 dstmac = "FF:FF:FF:FF:FF:FF"
 localip = "192.168.1.103"
-dstip = "192.168.1.1"
+dstip = sys.argv[1]
 
 result_raw = srp(Ether(dst = dstmac, src = localmac)/ARP(op=1, hwsrc=localmac, psrc=localip, pdst = dstip),iface="wlan0",verbose=False)
 result = result_raw[0]
