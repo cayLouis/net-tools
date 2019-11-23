11c08330a4e1a91d8d1c4b813718fde7631eb0de
cayLouis <815569735@qq.com>
2019-11-23 15:17:27 +0800

    add a new tool

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-from scapy.all import *
 
-response = srp(Ether(src="b0:35:9f:21:5e:4e", dst="FF:FF:FF:FF:FF:FF")/ARP(op=1, hwsrc="b0:35:9f:21:5e:4e", psrc="192.168.1.103", pdst="192.168.1.1"),iface="wlan0",verbose=False)
-print(response[0].res[0][1].getlayer(ARP).hwsrc)
+def tip(fun):
+    print("mkmk")
+    def wrapper(*lis, **dict):
+        print("start")
+        fun(*lis, **dict)
+        print("end")
+    return wrapper
+
+@tip
+def test(*a, **b):
+    print(len(a))
+
+a = tip(test)
+a(123)
---
+++ traceroute.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+import struct
+
+def ip_handle(src):
+    src_list = src.split(".")
+    for sec in src_list:
+        src_pact = struct.pack(">B", int(sec))
+        yield src_pact
+
+def traceroute(src, dst):
+    src_handle = ip_handle(src)
+    ip_options = b'\x07\x27\x08'+next(src_handle)+next(src_handle)+next(src_handle)\
+        +next(src_handle)+b'\x00'*33
+    pkt = IP(dst = dst, options=IPOption(ip_options))/ICMP(type=8, code=0)
+    result = sr1(pkt, timeout=60, verbose=False)
+    # for router in result.getlayer(IP).options[0].fields['routers']:
+    #     print(router)
+    print(result.getlayer(IP).show())
+
+traceroute("192.168.1.103","192.168.1.105")
+    
