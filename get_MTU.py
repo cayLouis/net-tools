027293e579f9710fa5df93a3d310b05498b69ad6
cayLouis <815569735@qq.com>
2019-12-03 17:38:25 +0800

    second

--- get_MTU.py
+++ get_MTU.py
@@ -5,12 +5,17 @@ import re
 """
 该程序用于获取目的ip路由的最小MTU值，防止IP包被分片
 """
+"""
+测试环境有限，可能因为路由过程中由于防火墙的作用，阻止了主机对“需要进行分片但设置了不分片比特”的差错控制报文
+的接收，导致接收到的报文均为None
+"""
 
 
 def response(dst, mtu):
+    # 设置填充数据大小，用mtu的值减去IP头部和ICMP头部的大小
     pyload = b'l' * (int(mtu) - 28)
 
-    ping_one_reply = sr1(IP(dst=dst, flags="DF") / ICMP() / pyload, timeout=1, verbose=False)
+    ping_one_reply = sr1(IP(dst=dst, flags="DF") / ICMP() / pyload, timeout=3, verbose=False)
     try:
         type_no = ping_one_reply.getlayer(ICMP).type
         code_no = ping_one_reply.getlayer(ICMP).code
@@ -18,18 +23,18 @@ def response(dst, mtu):
             return 1, mtu
         elif type_no == 0 and code_no == 0:
             return 2, mtu
-
     except Exception as e:
         if re.match(".*NoneType.*", str(e)):
             return None
 
 
 def discover_min_mtu(dst):
-    mtu = 1500
+    mtu = 1000
     while True:
         result = response(dst, mtu)
         if result is None:
             print("目标%s不可达" % dst)
+            break
         elif result[0] == 1:
             mtu = mtu - 10
             print("MTU: %s 测试不通过" % str(mtu))
